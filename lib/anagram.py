e29f69815db1cf31831440730c9bca7a717bdfa0
Silva-NK <kiagonsuzanne@gmail.com>
2025-05-22 19:36:23 +0300

    Completes anagram function.

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(self.word)
+
+    def match(self, words):
+        matches = []
+
+        for possible in words:
+            possible_lower = possible.lower()
+            if possible_lower != self.word and sorted(possible_lower) == self.sorted_word:
+                matches.append(possible)
+        return matches
+    
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'icelets', 'banana']))
